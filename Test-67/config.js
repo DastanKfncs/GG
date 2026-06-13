de1111b353bfcd4d626afc78abb74ea2dfbc6fe3
DastanKfncs <prime.dastan12@gmail.com>
2026-06-13 14:56:48 +0600

    Add files via upload

---
+++ Test-67/config.js
@@ -0,0 +1,14 @@
+export const config = {
+  weatherCity: 'Bishkek',
+  currencyBase: 'USD',
+  currencySymbols: ['EUR', 'RUB', 'KGS'],
+  newsApiKey: '74a652d1d51f40cda4c9c5e17a45f415', // Вставьте сюда API-ключ NewsAPI.org
+  newsQuery: 'Kyrgyzstan OR Бишкек OR Кыргызстан',
+  newsUrl: 'https://api.spaceflightnewsapi.net/v3/articles?_limit=7',
+  refreshIntervalMs: 60000,
+  cachePrefix: 'office-dashboard',
+  themeStorageKey: 'office-dashboard-theme',
+  apiTimeoutMs: 10000,
+};
+
+
